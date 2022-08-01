995b9017df8537a832be73007b45c9d2c265efbb
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-08-01 17:42:02 +0000

    Adding content

--- main.py
+++ main.py
@@ -29,4 +29,31 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+done = False
+money = 0.0
 
+def report():
+  print(f"Water: {resources['water']}ml")    
+  print(f"Milk: {resources['milk']}ml")
+  print(f"Coffee: {resources['coffee']}g")
+  print(f"Money: {money}")
+def check_resources(ingredients):
+  resources_needed = True
+  for resource in resources:
+    if resources[resource] < ingredients[resource]:
+      print("failed")
+      resources_needed = False
+  if resources_needed:
+    for resource in resources:
+      resources[resource] = resources[resource] - ingredients[resource]
+    print(f"Here is your {choice}!")
+
+
+while(done == False):
+  choice = input("What would you like (expresso/latte/cappuccino): ")
+  if choice == 'off':
+    done = True
+  elif choice == 'report':
+    report()
+  else:
+    check_resources(MENU[choice]['ingredients'])
