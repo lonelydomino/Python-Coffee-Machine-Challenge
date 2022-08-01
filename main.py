fb22f9dfb7dbfd3d444715f74734cf9975990b1e
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-08-01 21:32:24 +0000

    doing more

--- main.py
+++ main.py
@@ -29,31 +29,50 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+cash = {
+  "money": 0.0
+}
 done = False
-money = 0.0
 
 def report():
   print(f"Water: {resources['water']}ml")    
   print(f"Milk: {resources['milk']}ml")
   print(f"Coffee: {resources['coffee']}g")
-  print(f"Money: {money}")
+  print(f"Money: {cash['money']}")
+  
 def check_resources(ingredients):
-  resources_needed = True
   for resource in resources:
     if resources[resource] < ingredients[resource]:
-      print("failed")
-      resources_needed = False
-  if resources_needed:
-    for resource in resources:
-      resources[resource] = resources[resource] - ingredients[resource]
-    print(f"Here is your {choice}!")
+      print("Not enough resources")
+      return False  
+  return True  
+  
+def deduct_resources(ingredients, cost):
+  for resource in resources:
+    resources[resource] -= ingredients[resource]
+  cash['money'] -= cost
 
+def process_coins():
+  print("Please insert coins.")
+  total = int(input("How many quarters?")) * 0.25
+  total += int(input("How many dimes?")) * 0.1
+  total += int(input("How many nickles?")) * 0.05
+  total += int(input("How many pennies?")) * 0.01
+  return total
 
+  
 while(done == False):
-  choice = input("What would you like (expresso/latte/cappuccino): ")
+  choice = input("What would you like (espresso/latte/cappuccino): ")
   if choice == 'off':
     done = True
   elif choice == 'report':
     report()
   else:
-    check_resources(MENU[choice]['ingredients'])
+    drink = MENU[choice]
+    cash['money'] = process_coins()
+    # print(f"money is {money} and drink cost is {drink['cost']}")
+    if check_resources(drink['ingredients']) and cash['money'] >= drink['cost']:
+      deduct_resources(drink['ingredients'], drink['cost'])
+      print(f"Here is your {drink}!")
+    else:
+      print("Not enough resources.")
